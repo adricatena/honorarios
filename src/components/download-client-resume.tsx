5a03b7a18471034e567a56b0ee82c43b12574a2b
Adriano <adricatena@gmail.com>
2026-02-21 18:02:28 -0300

    feat: implement loading state for DownloadClientResume button

--- src/components/download-client-resume.tsx
+++ src/components/download-client-resume.tsx
@@ -3,6 +3,7 @@
 import type { Config } from '@/payload-types'
 import { PayloadSDK } from '@payloadcms/sdk'
 import { Button, useDocumentInfo } from '@payloadcms/ui'
+import { useState } from 'react'
 
 const sdk = new PayloadSDK<Config>({
   baseURL: '/api',
@@ -12,5 +13,17 @@ export function DownloadClientResume() {
   const documentInfo = useDocumentInfo()
   console.log('Document Info:', documentInfo)
 
-  return <Button>Descargar Cuenta Corriente</Button>
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClickDownloadResume = async () => {
+    setIsLoading(true)
+
+    setIsLoading(false)
+  }
+
+  return (
+    <Button disabled={isLoading} onClick={handleClickDownloadResume}>
+      Descargar Cuenta Corriente
+    </Button>
+  )
 }
